7f749d63e788085102a6754e78a3ad1bd49cc7ed
gfcapalbo <giovanni@therp.nl>
2016-05-31 12:44:45 +0200

    fixes

--- product_supplierinfo_for_customer/models/sale_order_line.py
+++ product_supplierinfo_for_customer/models/sale_order_line.py
@@ -2,7 +2,7 @@
 from openerp import models, api
 
 
-class SaleOrderLine2(models.Model):
+class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
 
@@ -12,16 +12,22 @@ class SaleOrderLine2(models.Model):
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False,
             fiscal_position=False, flag=False):
-        res = super(SaleOrderLine2, self).product_id_change(
+        res = super(SaleOrderLine, self).product_id_change(
                 pricelist, product, qty=qty, uom=uom,
                 qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
             	lang=lang, update_tax=update_tax, date_order=date_order, 
 		packaging=packaging, fiscal_position=fiscal_position, 
                 flag=flag)
         product_rec = self.env['product.product'].browse(product)
-        if res.get('value', {}).get('name') and product_rec.customer_ids.ids:
-            pc = product_rec.customer_ids.product_code
-            pn = product_rec.customer_ids.product_name
-            if pn:
-                res['value']['name'] = pc + '--' + pn
+        partner_rec = self.env['res.partner'].browse(partner_id)
+        if product_rec.customer_ids:
+            selected_supplier_info = product_rec.customer_ids.filtered(
+                lambda x: x.partner_id == partner_rec)[:1]
+            if res.get('value', {}).get('name') and selected_supplier_info:
+                pc = product_rec.customer_ids.product_code + '--'
+                pn = product_rec.customer_ids.product_name
+                if pn and pc:
+                    res['value']['name'] = pc + pn
+                if pn:
+                    res['value']['name'] = pn
         return res
