afabbcdda5208ee8007c4ca4650f88c4728a4c25
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-05-10 14:47:36 +0200

    fix

--- product_tag/models/product_tag.py
+++ product_tag/models/product_tag.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
-
+from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
 
 class ProductTag(models.Model):
     _name = 'product.tag'
@@ -21,14 +21,16 @@ class ProductTag(models.Model):
 
     name = fields.Char('Category Name', required=True, translate=True)
     parent_id = fields.Many2one(
-        comodel_name='product.tag', 'Parent tag', select=True,  ondelete='cascade')
+        comodel_name='product.tag', string='Parent tag',
+        select=True,  ondelete='cascade'
+    )
     child_ids = fields.One2many('product.tag', 'parent_id', 'Child Categories')
     active= fields.Boolean('Active', default=True)
     product_ids = fields.Many2many('product.template', string='Products')
 
     api.constrains('parent_id')
     def _check_parent_id(self):
-         if not self._check_recursion():
+        if not self._check_recursion():
             raise ValidationError(_(
                 'Error ! You can not create recursive tags.')
             )
